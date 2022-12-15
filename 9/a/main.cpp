cf9cba4013fced79ca89ed18966900903c7d5d04
Scaniox <alexl.mills3@gmail.com>
2022-12-15 21:45:08 +0000

    started day 9

---
+++ 9/a/main.cpp
@@ -0,0 +1,22 @@
+/*******************************************************************************
+ *         File: Advent-of-Code-2022\9\a\main.cpp 
+ *       Author: Alexander Mills (Scaniox#7130)
+ *      License: 
+ *         Date: 
+ *  Description: 
+ ******************************************************************************/
+#include <iostream>
+#include <fstream>
+#include <string>
+
+int main() {
+    std::string line;
+    std::fstream input_file;
+    input_file.open("test.txt");
+
+    std::array<int, 2> head{1,2};
+    
+    while(getline(input_file, line)) {
+
+    }
+}
